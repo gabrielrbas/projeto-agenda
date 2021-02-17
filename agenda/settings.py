7bc1b865e762a023f36d38e2db64f1fbfab865bf
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-02-17 01:17:22 -0300

    Erros em settings.py corrigidos e servidor inicializado

--- agenda/settings.py
+++ agenda/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contatos.app.ContatosConfig',
+    'contatos.apps.ContatosConfig',
 ]
 
 MIDDLEWARE = [
